8328fbf0c1974bedb1112d43dd5f227df0636eb7
Haoshuang Wang <haoshuang@flatironschool.com>
2017-07-13 04:18:32 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -8,7 +8,6 @@
   #{}"-----------"
 #end
 #board = [" "," "," "," "," "," "," "," "," "]
-#board=[" "," "," "," "," "," "," "," "," "]
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     false
