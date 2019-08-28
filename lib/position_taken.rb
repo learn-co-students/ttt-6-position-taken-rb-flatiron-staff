9571009994a7b810aac240fcff64e6b756c4c565
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-08-28 01:25:42 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+  if board[index] == " " or board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif ["X", "O"].include? board[index]
+    true
+  end
+end
